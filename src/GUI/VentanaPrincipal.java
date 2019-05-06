d841383f056d2dbde744220ac3a92ae0a50eb309
David <david.lozanoc@um.es>
2019-05-06 08:21:05 +0200

    Update 1

--- src/GUI/VentanaPrincipal.java
+++ src/GUI/VentanaPrincipal.java
@@ -1,12 +1,45 @@
 package GUI;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class VentanaPrincipal {
-    private JPanel panel;
-    private JTextArea OTRIManagerTextArea;
+    private JPanel panelPrincipal;
+    private JTextPane textPane;
+    private JLabel bienvenida;
+    private JButton crearContratoButton;
+    private JButton actualizarContratoButton;
+    private JButton prorrogarContratoButton;
+    private JButton consultarSuperficieButton;
+    private JButton consultarContratosButton;
+    private JButton consultarPresupuestosButton;
+    private JButton consultarInvestigadoresButton;
+    private JButton consultarEmpresariosButton;
+    private JButton compararAsociadosButton;
+    private JButton resumenGlobalButton;
+    private JButton iniciarSesionButton;
+    private JButton cerrarSesionButton;
 
-    public JPanel getPanel() {
-        return this.panel;
+    private VentanaPrincipal() {
+        iniciarSesionButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                IniciarSesion dialog = new IniciarSesion();
+                dialog.pack();
+                dialog.setLocationRelativeTo(null);
+                dialog.setVisible(true);
+            }
+        });
+    }
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("OTRI Manager");
+        frame.setContentPane(new VentanaPrincipal().panelPrincipal);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setSize(800, 600);
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
     }
 }
